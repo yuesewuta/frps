d86fd0adbfcd8e7909834e244481a5e2f7f4bf73
Alan <59946363+yuesewuta@users.noreply.github.com>
2023-12-12 14:49:43 +0800

    Update makesure.sh

--- makesure.sh
+++ makesure.sh
@@ -1,9 +1,9 @@
 echo "此次文档为b站ZhiaoC于 2023年12月12日测试 欢迎咨询和支持作者"
 echo "感谢frp作者 fatedier https://afdian.net/a/fatedier"
 echo "感谢docker image作者 snowdream"
-echo "感谢饥荒一键开服作者 b站 长鸽门徒"
-
-echo "此次文档为b站ZhiaoC于 2023年12月12日测试 欢迎咨询和支持作者"
+echo "感谢饥荒一键开服作者 b站 长鸽门徒" &&
+wget -O makesure.sh https://raw.githubusercontent.com/yuesewuta/frps/main/showme.png && 
+echo "此次文档为b站ZhiaoC于 2023年12月12日测试 欢迎咨询和支持作者\n"
 echo "此次文档为b站ZhiaoC于 2023年12月12日测试 欢迎咨询和支持作者"
 echo "此次文档为b站ZhiaoC于 2023年12月12日测试 欢迎咨询和支持作者"
 catimg image.png && echo -e "\n\t\t\\t\t\t\e[36m支付宝\t\t\t\t\t\t\t\t\t\e[32m微信\e[0m\n"
