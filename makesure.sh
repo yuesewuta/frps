26b328954e6096ee6d0c4f747e1c2980257c506c
Alan <59946363+yuesewuta@users.noreply.github.com>
2023-12-12 11:22:25 +0800

    Update makesure.sh

--- makesure.sh
+++ makesure.sh
@@ -2,3 +2,7 @@ echo "此次文档为b站ZhiaoC于 2023年12月12日测试 欢迎咨询和支持
 echo "感谢frp作者 fatedier https://afdian.net/a/fatedier"
 echo "感谢docker image作者 snowdream"
 echo "感谢饥荒一键开服作者 b站 长鸽门徒"
+
+echo "此次文档为b站ZhiaoC于 2023年12月12日测试 欢迎咨询和支持作者"
+echo "此次文档为b站ZhiaoC于 2023年12月12日测试 欢迎咨询和支持作者"
+echo "此次文档为b站ZhiaoC于 2023年12月12日测试 欢迎咨询和支持作者"
